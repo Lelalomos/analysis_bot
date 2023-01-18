350ca420a9af6629862a59a451acf0aec252ba94
Lelalomos <lelalomos@gmail.com>
2023-01-19 02:59:07 +0700

    add lab and update code

--- trade.py
+++ trade.py
@@ -1,6 +1,5 @@
-from talib.abstract import EMA, STOCH
-import matplotlib.pyplot as plt
-import os
+from talib.abstract import EMA, MACD
+from utils import plot_save_img
 
 def cross_ema(df, namest, **config):
     """
@@ -18,36 +17,101 @@ def cross_ema(df, namest, **config):
 
     low_span = config['cross_ema']['low_span']
     long_span = config['cross_ema']['long_span']
-    low_df = EMA(df['close'], low_span)
-    long_df = EMA(df['close'], long_span)
+    # low_df = EMA(df['close'], low_span)
+    # long_df = EMA(df['close'], long_span)
+    df['low_df'] = EMA(df['close'], low_span)
+    df['long_df'] = EMA(df['close'], long_span)
 
-    if config['plot_config']['plot']:
-        fig = plt.figure(figsize=(10, 7))
-        plt.title(f'cross_ema-{namest}')
-        plt.plot(low_df[config['plot_config']['n_bar']*-1:], 'g', label= f'ema{low_span}')
-        plt.plot(long_df[config['plot_config']['n_bar']*-1:], 'r', label= f'ema{long_span}')
-        leg = plt.legend(loc='upper left')
-        plt.savefig(os.path.join(os.getcwd(),'images',f'cross_ema-{namest}.jpg'))
+    low_line = list(df['low_df'])
+    long_line = list(df['long_df'])
 
+    plot_save_img(long_line, low_line, namest)
 
     # logic buy and sell
     #     buy 100>200
-    if low_df[-1] > long_df[-1]:
-        return 0 #False
+    print('test:',low_line[-1])
+    if low_line[-1] > long_line[-1]:
+        return 2, df #False
     #     sell 100<200
-    elif low_df[-1] < long_df[-1]:
-        return 1 #True
-    return 3
+    elif low_line[-1] < long_line[-1]:
+        return -1, df #True
+    return 0, df
 
 
-def ema_stoch(df):
-    ...
+def ichimoku_cloud(df, tenkansen_value = 9, kinjunsen_value = 26, shift_value = 26, senkou_b_value = 52, namest = "empty"):
+        """
+        Description:
+            Get the values of Lines for Ichimoku Cloud
 
-def ichimoku(df):
-    ...
+        Args:
+            df: Dataframe,
+            int: tenkansen value,
+            int: kinjunsen value,
+            int: shift value,
+            int: senkou_b value,
 
-def macd_stoch(df):
-    ...
+        Returns:
+            df: Dataframe
 
-def macd(df):
-    ...
+        Reference Code: https://stackoverflow.com/questions/28477222/python-pandas-calculate-ichimoku-chart-components
+        
+        """
+
+        # Tenkan-sen (Conversion Line): (9-period high + 9-period low)/2))
+        period9_high = df['high'].rolling(window=tenkansen_value).max()
+        period9_low = df['low'].rolling(window=tenkansen_value).min()
+        tenkan_sen = (period9_high + period9_low) / 2
+
+        # Kijun-sen (Base Line): (26-period high + 26-period low)/2))
+        period26_high = df['high'].rolling(window=kinjunsen_value).max()
+        period26_low = df['low'].rolling(window=kinjunsen_value).min()
+        kijun_sen = (period26_high + period26_low) / 2
+
+        # Senkou Span A (Leading Span A): (Conversion Line + Base Line)/2))
+        senkou_span_a = ((tenkan_sen + kijun_sen) / 2).shift(shift_value)
+
+        # Senkou Span B (Leading Span B): (52-period high + 52-period low)/2))
+        period52_high = df['high'].rolling(window=senkou_b_value).max()
+        period52_low = df['low'].rolling(window=senkou_b_value).min()
+        senkou_span_b = ((period52_high + period52_low) / 2).shift(shift_value)
+
+        df['cloud_green_line_a'] = senkou_span_a
+        df['cloud_red_line_b'] = senkou_span_b
+
+        # plot img
+        plot_save_img(df['cloud_green_line_a'], df['cloud_red_line_b'], namest)
+
+        # logic buy and sell
+        close = df['close'].to_list()[-1]
+        cloud_green_line_a =    df['cloud_green_line_a'].to_list()[-1]
+        cloud_red_line_b =  df['cloud_red_line_b'].to_list()[-1]
+
+        close = float(close)
+        cloud_green_line_a = float(cloud_green_line_a)
+        cloud_red_line_b = float(cloud_red_line_b)
+
+        # buy
+        if close > cloud_green_line_a and close > cloud_red_line_b and cloud_green_line_a > cloud_red_line_b:
+            return 2, df
+        # sell
+        elif close < cloud_green_line_a and close < cloud_red_line_b and cloud_green_line_a < cloud_red_line_b:
+            return -1, df
+        # noting
+        else:
+            return 0, df       
+
+def macd(df, namest):
+    df['macd_line'], df["signal_line"], _ = MACD(df['close'])
+    macd_line = list(df['macd_line'])
+    signal_line = list(df["signal_line"])
+
+    # config plot
+    plot_save_img(macd_line, signal_line, namest)
+        
+    #     buy
+    if macd_line[-1] > signal_line[-1]:
+        return 2, df #False
+    #     sell
+    elif macd_line[-1] < signal_line[-1]:
+        return -1, df #True
+    return 0, df
--- utils.py
+++ utils.py
@@ -1,3 +1,23 @@
+import json
+import os
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def read_config(path_json):
+    """
+
+    Args:
+        path_json (string): path json file
+
+    Returns:
+        dictionary: data of json file
+
+    """
+    with open(path_json) as js:
+        return json.load(js)
+
+# read config for plot image from plot_image folder
+config_plot = read_config(os.path.join(os.getcwd(),'config','plot_image','plot_config.json'))
 
 def loop_yield(list_v,step=5):
     for i in range(0,len(list_v),step):
@@ -11,3 +31,13 @@ def search_stock_th(name_stock, tv, q):
             q.put('found')
 
     return q.put(name_stock)
+
+def plot_save_img(green_line, red_line, namest):
+    if config_plot['plot_config']['plot']:
+        os.makedirs(os.path.join(os.getcwd(),'images',f'macd-{namest}'),exist_ok=True)
+        plt.figure(figsize=(10, 7))
+        plt.title(f'macd-{namest}')
+        plt.plot(green_line[config_plot['plot_config']['n_bar']*-1:], 'g', label= f'green line')
+        plt.plot(red_line[config_plot['plot_config']['n_bar']*-1:], 'r', label= f'red line')
+        plt.legend(loc='upper left')
+        plt.savefig(os.path.join(os.getcwd(),'images',f'macd-{namest}-{datetime.now().strftime("%Y%m%d")}.jpg'))
