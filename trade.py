66310a7e464da7efd7c567df74bd15d3b2ac5611
Lelalomos <lelalomos@gmail.com>
2023-01-21 02:32:06 +0700

    add mtf ssl channel + volume price trend and update lab

--- trade.py
+++ trade.py
@@ -1,5 +1,6 @@
 from talib.abstract import EMA, MACD
 from utils import plot_save_img
+import numpy as np
 
 def cross_ema(df, namest, **config):
     """
@@ -114,3 +115,65 @@ def macd(df, namest):
     elif macd_line[-1] < signal_line[-1]:
         return -1, df #True
     return 0, df
+
+
+def mtf_ssl(dataframe, length=250):
+    df = dataframe.copy()
+
+    df["smaHigh"] = df["high"].rolling(length).mean()
+    df["smaLow"] = df["low"].rolling(length).mean()
+
+    df["hlv"] = np.where(
+        df["close"] > df["smaHigh"], 1, np.where(df["close"] < df["smaLow"], -1, np.NAN)
+    )
+    df["hlv"] = df["hlv"].ffill()
+
+    df["sslDown"] = np.where(df["hlv"] < 0, df["smaHigh"], df["smaLow"])
+    df["sslUp"] = np.where(df["hlv"] < 0, df["smaLow"], df["smaHigh"])
+
+    return df["sslDown"], df["sslUp"]
+
+
+def pvt_with_divergence(df,short_length = 90, long_length = 95):
+
+    # Compute PVT
+    pvt = (df['volume'] * (df['close'] - df['close'].shift(1)) / df['close']).cumsum()
+
+    # Compute OBV Oscillator
+    short_pvt = EMA(pvt, timeperiod=short_length)
+    long_pvt = EMA(pvt, timeperiod=long_length)
+    pvt_osc = short_pvt - long_pvt
+
+    # Compute divergence
+    up = (df['close'].pct_change().where(df['close'].pct_change() > 0, other=0)).rolling(window=short_length).mean()
+    down = (-df['close'].pct_change().where(df['close'].pct_change() < 0, other=0)).rolling(window=short_length).mean()
+    # down = (-(df['close'] - df['close'].shift(1) < 0)).rolling(window=short_length).mean()
+
+    return up, down, pvt_osc
+
+
+def normalize_list(nums):
+    min_val = np.min(nums)
+    max_val = np.max(nums)
+    return np.divide(np.subtract(nums, min_val), (max_val - min_val))
+
+
+def collect_mtfssl_pvtdiver(df, short_ema, long_ema):
+    pvtup, pvtdown, pvt_osc = pvt_with_divergence(df, short_ema, long_ema)
+    pvt_osc = pvt_osc[~np.isnan(pvt_osc)]
+    pvtup = pvtup.dropna()
+    pvtdown = pvtdown.dropna()
+    normailize_data = normalize_list(pvt_osc)
+    sslup, ssldown = mtf_ssl(df)
+    sslup = sslup.dropna()
+    ssldown = ssldown.dropna()
+    # buy
+    if sslup.to_list()[-1] > ssldown.to_list()[-1] and normailize_data[-1] > pvtup.to_list()[-1]:
+        return 2 #False
+    #     sell
+    elif sslup.to_list()[-1] < ssldown.to_list()[-1] and normailize_data[-1] < pvtdown.to_list()[-1]:
+        return -1 #True
+    return 0
+
+
+
