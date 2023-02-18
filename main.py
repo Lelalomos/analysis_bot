2883d627678accc78245cc946df4b023d9886b2b
Lelalomos <lelalomos@gmail.com>
2023-02-19 00:58:35 +0700

    update main.py

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ with open(os.path.join(os.getcwd(),'config','config.json')) as f:
 assert config is not None, "error read config"
 
 current_date = datetime.datetime.today().date()
+list_time = [62, 93]
 
 
 for days in config['len_data']:
@@ -52,6 +53,9 @@ for days in config['len_data']:
         dict_stock_name_score = {}
         for namest2dict in json_stock['list_stock'][key_exc]:
             dict_stock_name_score.update({namest2dict:0})
+            dict_remaining_date.update({namest2dict:{}})
+            for t in list_time:
+                dict_remaining_date[namest2dict].update({f"{t}":0})
 
         for namest in json_stock['list_stock'][key_exc]:
             try:
@@ -119,7 +123,6 @@ for days in config['len_data']:
             # df = df[df['close'] == min(list(df['close']))] 
 
             # find date with the least value in data. do it every month
-            list_time = [62, 93]
             for t in list_time:
                 data_follow_time = get_data(tv, key_exc, namest, t)
                 data_follow_time = data_follow_time.reset_index()
@@ -139,18 +142,41 @@ for days in config['len_data']:
                 df_min_value = data_follow_time[data_follow_time['close'] == min(list(data_follow_time['close']))]
                 df_current_value = data_follow_time.iloc[-2:-1,:]
                 if t == 62:
+                    dict_remaining_date[namest][f'{t}'] = remaining_date.days
                     dict_min_value_1[namest] = df_current_value['close'].values[0] - min(list(data_follow_time['close']))
-                    dict_remaining_date[namest] = remaining_date.days
                 else:
+                    dict_remaining_date[namest][f'{t}'] = remaining_date.days
                     dict_min_value_2[namest] = df_current_value['close'].values[0] - min(list(data_follow_time['close']))
-                    dict_remaining_date[namest] = remaining_date.days
-
-
+                
         # sort data by current close value minus the smallest a value in the past
         dict_min_value_1_sort = dict(sorted(dict_min_value_1.items(), key=lambda item: item[1], reverse=True))
         dict_min_value_2_sort = dict(sorted(dict_min_value_2.items(), key=lambda item: item[1], reverse=True))
-        # sort data remaining date
-        dict_remaining_date_sort = dict(sorted(dict_remaining_date.items(), key=lambda item: item[1], reverse=True))
+
+        # increse score following by date
+        dict_prepare_sort = {}
+        score_follow_time = 0.5
+        for data_v in list_time:
+            for key,_ in dict_remaining_date.items():
+                dict_prepare_sort[key] = dict_remaining_date[key][f"{data_v}"]
+            
+            dict_remaining_date_sort = dict(sorted(dict_prepare_sort.items(), key=lambda item: item[1],reverse=True))
+            list_score = [i+score_follow_time for i in range(10)]
+
+            reverse_date_tomin_first = dict(sorted(dict_remaining_date_sort.items(), key=lambda item: item[1]))
+            print('min close value of number of day: ',data_v)
+            for k,v in reverse_date_tomin_first.items():
+                if int(v) <= 10:
+                    dict_pair_daywithscore['min_date'].append(k)
+                    print(k,v)
+            print('-'*100)
+
+            for add_score, key_score in zip(list_score,list(dict_remaining_date_sort.keys())[:10]):
+                dict_stock_name_score[key_score] = dict_stock_name_score[key_score] + add_score
+
+            score_follow_time+=0.5
+            
+
+        # print('dict_remaining_date_sort:',dict_remaining_date_sort)
 
         # print('-'*100)
         # print('dict_min_value_1_sort:',dict_min_value_1_sort)
@@ -159,29 +185,47 @@ for days in config['len_data']:
         # print('result step 1:',dict_stock_name_score)
 
         # loop increase score
-        for dict_sort_value in [dict_min_value_1_sort, dict_min_value_2_sort, dict_remaining_date_sort]:
+        for dict_sort_value in [dict_min_value_1_sort, dict_min_value_2_sort]:
+            # print('dict_sort_value:',dict_sort_value)
             # normalize score
-            list_score = [i*100 for i in range(1,len(dict_sort_value)+1)]
+            list_score = [i*1000 for i in range(1,len(dict_sort_value)+1)]
             np_score = np.array(list_score)
             normalized_arr = preprocessing.normalize([np_score])
+            # print('normalize:',normalized_arr.tolist()[0])
 
             dict_score = {} 
             for score,(k,v) in zip(normalized_arr.tolist()[0],dict_sort_value.items()):
                 dict_score.update({k:score})
 
+            # print('dict_score:',dict_score)
             for k,v in dict_score.items():
                 dict_stock_name_score[k] = dict_stock_name_score[k] + dict_score[k]
 
-        print('score:',dict_stock_name_score)
 
+        # print('score:',dict_stock_name_score)
         dict_stock_name_score_sort = dict(sorted(dict_stock_name_score.items(), key=lambda item: item[1], reverse=True))
 
-        print('dict_stock_name_score_sort final:',dict_stock_name_score_sort)
-        
+        # print('dict_stock_name_score_sort final:',dict_stock_name_score_sort)
+        dict_pair_daywithscore = {
+            "stock_score":[],
+            "min_date":[]
+        }
         print(f'days: {days}')
         print('-'*100)
+        int_check = 0
         for k,v in dict_stock_name_score_sort.items():
+            dict_pair_daywithscore['stock_score'].append(k)
             print(k,v)
+            int_check+=1
+            if int_check >=10:
+                break
         print('-'*100)
 
-            
+        # pair
+        print('... date and score ...')
+        dict_pair_daywithscore['stock_score'] = dict_pair_daywithscore['stock_score'][:10]
+        for stock_sc in dict_pair_daywithscore['stock_score']:
+            if stock_sc in dict_pair_daywithscore['min_date']:
+                print(stock_sc)
+        
+        print('-'*100)
