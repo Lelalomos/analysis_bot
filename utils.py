9a6c14ca1d8ef4c3cd5ad7d82940b0b25d7089aa
lelalomos <lelalomos@gmail.com>
2023-04-13 18:36:32 +0700

    support fund analysis, optimize code, get data and store data, not pull duplicate data, upgrade pandas version to 2.0

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import json
 import os
 import matplotlib.pyplot as plt
 from datetime import datetime
+from tvDatafeed import Interval
+import pandas as pd
 
 def read_config(path_json):
     """
@@ -41,3 +43,54 @@ def plot_save_img(green_line, red_line, namest):
         plt.plot(red_line[config_plot['plot_config']['n_bar']*-1:], 'r', label= f'red line')
         plt.legend(loc='upper left')
         plt.savefig(os.path.join(os.getcwd(),'images',f'macd-{namest}-{datetime.now().strftime("%Y%m%d")}.jpg'))
+
+
+# timeout: _ssl.c:1112: The handshake operation timed out
+def get_data(tv, nav, exchange, name, n_bars, mode):
+    # prepare folder to store data
+    os.makedirs(os.path.join(os.getcwd(),'data'),exist_ok=True)
+    os.makedirs(os.path.join(os.getcwd(),'data',mode),exist_ok=True)
+    os.makedirs(os.path.join(os.getcwd(),'data',mode,exchange),exist_ok=True)
+    # os.makedirs(os.path.join(os.getcwd(),'data','crypto'),exist_ok=True)
+
+    # get data
+    getd = None
+    if mode == "stock":
+        try:
+            getd = tv.get_hist(name, exchange, interval=Interval.in_daily, n_bars=n_bars)
+            getd = getd.reset_index()
+        except Exception as e:
+            print('[stock] error get data:',e)
+            return []
+    elif mode == "fund":
+        try:
+            getd = nav.get_all(name, asDataFrame=True)
+            getd = getd.drop(["tags","fund"],axis=1)
+            # prepare data to same format with stock
+            getd = getd.rename(columns = {'value':'close','updated':'datetime'})
+        except Exception as e:
+            print('[fund] error get data:',e)
+            return []
+    elif mode == "crypto":
+        pass
+
+    # save data to parquet when data more than one
+    if len(getd)>0:
+        getd.to_parquet(os.path.join(os.getcwd(), 'data', mode, exchange, f"{name}.parquet"))
+
+    return getd
+
+
+# load data
+def load_data(name, mode, exchange, time):
+    try:
+        loadd = pd.read_parquet(os.path.join(os.getcwd(),'data', mode, exchange ,f"{name}.parquet"))
+        if len(loadd) > 0:
+            return loadd.iloc[:time,:]
+        else:
+            return []
+    except Exception as e:
+        print('error load data from parquet file:',e)
+        return []
+
+        
